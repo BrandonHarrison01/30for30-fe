bf43f2bb94a8a3f8aa1617aae6fbf148dcba686e
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2020-02-07 16:43:21 -0700

    created login component

---
+++ src/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Login(){
+    return(
+        <div>
+            <h1>Username</h1>
+            <input/>
+            <h1>Password</h1>
+            <input/>
+        </div>
+    )
+}
+
+export default Login
