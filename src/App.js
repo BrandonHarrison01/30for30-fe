6ff3781ab40caba583f043a53ba73eee5e96cc83
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2020-02-18 23:34:15 -0700

    register page is rendered on app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 
-import Login from './Login'
+import Login from './components/Login'
+import Register from './components/Register'
 
 function App() {
   return (
     <div className="App">
       <h1>30for30 Front End</h1>
       <Login />
+      <Register />
     </div>
   );
 }
