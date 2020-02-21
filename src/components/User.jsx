04b86c789393d17ecb2f177a6c0070f315baaa60
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2020-02-20 23:16:51 -0700

    added decription to category

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -8,13 +8,24 @@ function User(){
     useEffect(() => {
         axiosWithAuth()
             .get('https://thirty-before-thirty-bw.herokuapp.com/api/user-items')
-            .then(res => console.log(res))
+            .then(res => {
+                console.log(res)
+                setUserData(res.data)
+            })
             .catch(err => console.log(err))
     }, [])
 
         return(
             <div>
-                <h1>user</h1>
+                <h1>Personal Goals</h1>
+                {userData.map(card => 
+                    <div>
+                        <p>category {card.category_name}</p>
+                        <p>{card.item_name}</p>
+                        <p>{card.description}</p>
+                        <p>{card.target_date}</p>
+                    </div>
+                )}
             </div>
         )
 
