0f3d7a0204343c601bedd093b74e8683cdcc2fa0
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2020-03-20 22:10:50 -0600

    able to rerender list of goals after delete

--- src/components/private/Goal.jsx
+++ src/components/private/Goal.jsx
@@ -2,22 +2,13 @@ import React from 'react'
 import { axiosWithAuth } from '../../axiosAuth'
 
 function Goal(props){
-
-    const handleDelete = id => {
-        axiosWithAuth()
-            .delete(`https://thirty-before-thirty-bw.herokuapp.com/api/remove-item/${id}`)
-            .then(res => console.log(res))
-            .catch(err => console.log(err))
-        // console.log(id, 'here is id')
-    }
-
     return(
         <div>
             <p>category {props.card.category_name}</p>
             <p>{props.card.item_name}</p>
             <p>{props.card.description}</p>
             <p>{props.card.target_date}</p>
-            <button onClick={() => handleDelete(props.card.id)}>Delete</button>
+            <button onClick={() => props.handleDelete(props.card.id)}>Delete</button>
         </div>
     )
 }
--- src/components/private/UserGoals.jsx
+++ src/components/private/UserGoals.jsx
@@ -1,62 +1,70 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { axiosWithAuth } from '../../axiosAuth'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { axiosWithAuth } from "../../axiosAuth";
 
-import NewGoalForm from './NewGoalForm'
+import NewGoalForm from "./NewGoalForm";
 
-import Goal from './Goal'
+import Goal from "./Goal";
 
-function UserGoals(props){
-    let [newGoal, setNewGoal] = useState({
-        item_name: "",
-        description: "",
-        category_id: 0,
-        privacy: 1,
-        target_date: ""
-      });
+function UserGoals(props) {
+  let [newGoal, setNewGoal] = useState({
+    item_name: "",
+    description: "",
+    category_id: 0,
+    privacy: 1,
+    target_date: ""
+  });
 
-    let [userData, setUserData] = useState([])
-    let [toggle, setToggle] = useState(0)
+  let [userData, setUserData] = useState([]);
+  let [toggle, setToggle] = useState(0);
 
-    useEffect(() => {
-        axiosWithAuth()
-            .get('https://thirty-before-thirty-bw.herokuapp.com/api/user-items')
-            .then(res => setUserData(res.data))
-            .catch(err => console.log(err))
-    }, [ toggle ])
+  useEffect(() => {
+    axiosWithAuth()
+      .get("https://thirty-before-thirty-bw.herokuapp.com/api/user-items")
+      .then(res => setUserData(res.data))
+      .catch(err => console.log(err));
+  }, [toggle]);
 
-    const submitNewGoal = e => {
-        e.preventDefault();
-        axiosWithAuth()
-            .post('https://thirty-before-thirty-bw.herokuapp.com/api/items', newGoal)
-            .then(res => console.log(res))
-            .catch(err => console.log(err))
-        setToggle(toggle === 0 ? 1 : 0)
-        setNewGoal({
-            item_name: "",
-            description: "",
-            category_id: 0,
-            privacy: 1,
-            target_date: ""
-          })
-    };
+  const submitNewGoal = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("https://thirty-before-thirty-bw.herokuapp.com/api/items", newGoal)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+    setToggle(toggle === 0 ? 1 : 0);
+  };
 
-    const logout = () => {
-        console.log('logging out')
-        props.history.push('/')
-        localStorage.removeItem('token')
-    }
+  const logout = () => {
+    console.log("logging out");
+    props.history.push("/");
+    localStorage.removeItem("token");
+  };
 
-    return(
-        <div>
-            <h1>Personal Goals</h1>
-            <Link to='/feed'>Public Goals</Link>
-            {userData.map(card => <Goal card={card}  key={card.id}/> )}
-            <NewGoalForm submitNewGoal={submitNewGoal} setNewGoal={setNewGoal} newGoal={newGoal} />
-            <button onClick={logout}>Logout</button>
-        </div>
-    )
+  const handleDelete = id => {
+    axiosWithAuth()
+      .delete(
+        `https://thirty-before-thirty-bw.herokuapp.com/api/remove-item/${id}`
+      )
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+    setToggle(toggle === 0 ? 1 : 0);
+  };
 
+  return (
+    <div>
+      <h1>Personal Goals</h1>
+      <Link to='/feed'>Public Goals</Link>
+      {userData.map(card => (
+        <Goal card={card} key={card.id} handleDelete={handleDelete} />
+      ))}
+      <NewGoalForm
+        submitNewGoal={submitNewGoal}
+        setNewGoal={setNewGoal}
+        newGoal={newGoal}
+      />
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
 }
 
-export default UserGoals
+export default UserGoals;
