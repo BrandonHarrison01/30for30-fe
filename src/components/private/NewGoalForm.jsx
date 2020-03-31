280e96a0b2f9e4b633393640574add9c8581e314
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2020-03-31 12:43:26 -0600

    able to toggle new category input and add new category

--- src/components/private/NewGoalForm.jsx
+++ src/components/private/NewGoalForm.jsx
@@ -3,17 +3,30 @@ import { axiosWithAuth } from "../../axiosAuth";
 
 function NewGoalForm(props) {
   let [categories, setCategories] = useState([]);
+  let [toggleInput, setToggleInput] = useState(0);
+  let [newCategory, setNewCategory] = useState('')
+  let [categoryLength, setCategoryLength] = useState(0)
+  // console.log(newCategory)
 
   useEffect(() => {
     axiosWithAuth()
       .get("https://thirty-before-thirty-bw.herokuapp.com/api/categories")
       .then(res => setCategories(res.data))
       .catch(err => console.log(err, "err"));
-  }, []);
+  }, [ categoryLength ]);
 
   const changeHandler = e => {
+    e.preventDefault();
+    props.setNewGoal({ ...props.newGoal, [e.target.name]: e.target.value });
+  };
+
+  const submitNewCategory = e => {
     e.preventDefault()
-    props.setNewGoal({ ...props.newGoal, [e.target.name]: e.target.value })
+    axiosWithAuth()
+      .post('https://thirty-before-thirty-bw.herokuapp.com/api/categories', {category_name: newCategory})
+      .then(res => setCategoryLength(res.data.length))
+      .catch(err => console.log(err))
+    setToggleInput(0)
   }
 
   return (
@@ -25,13 +38,13 @@ function NewGoalForm(props) {
           name='item_name'
           onChange={changeHandler}
           value={props.newGoal.item_name}
-          />
+        />
         <input
           type='text'
           name='description'
           onChange={changeHandler}
           value={props.newGoal.description}
-          />
+        />
         <input
           type='text'
           name='target_date'
@@ -43,15 +56,19 @@ function NewGoalForm(props) {
             <input
               type='radio'
               name='privacy'
-              onClick={e => props.setNewGoal({ ...props.newGoal, [e.target.name]: 0 })}
-              />
+              onClick={e =>
+                props.setNewGoal({ ...props.newGoal, [e.target.name]: 0 })
+              }
+            />
             Public
           </label>
           <label>
             <input
               type='radio'
               name='privacy'
-              onClick={e => props.setNewGoal({ ...props.newGoal, [e.target.name]: 1 })}
+              onClick={e =>
+                props.setNewGoal({ ...props.newGoal, [e.target.name]: 1 })
+              }
               defaultChecked
             />
             Private
@@ -59,17 +76,33 @@ function NewGoalForm(props) {
         </div>
         <select
           onChange={e =>
-            props.setNewGoal({...props.newGoal, category_id: parseInt(e.target.value)})
+            props.setNewGoal({
+              ...props.newGoal,
+              category_id: parseInt(e.target.value)
+            })
           }
         >
-          <option defaultValue >Select</option>
+          <option defaultValue>Select</option>
           {categories.map(category => (
             <option key={category.id} value={category.id}>
               {category.category_name}
             </option>
           ))}
         </select>
-        <button onClick={props.submitNewGoal}>click</button>
+        {toggleInput ? (
+          <div>
+            <input 
+              type='text'
+              onChange={e => setNewCategory(e.target.value)}
+            />
+            <button onClick={submitNewCategory}>Add</button>
+            <button onClick={() => setToggleInput(0)}>X</button>
+          </div>
+        ) : (
+          <button onClick={() => setToggleInput(1)}>New Category</button>
+        )}
+        <br />
+        <button onClick={props.submitNewGoal}>Submit New Goal</button>
       </form>
     </div>
   );
