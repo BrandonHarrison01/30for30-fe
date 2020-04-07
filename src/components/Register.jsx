8aa2e45b76a8585f061d902ce227eb708ac4ce3e
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2020-04-07 17:34:06 -0600

    error handling for registering new user

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -10,6 +10,7 @@ function Register(props) {
 
   let [password, setPassword] = useState('');
   let [confirmPassword, setConfirmPassword] = useState('');
+  let [error, setError] = useState()
 
   const handleSubmit = event => {
     event.preventDefault();
@@ -25,42 +26,45 @@ function Register(props) {
             localStorage.setItem('token', res.data.token);
             props.history.push('/')
           })
-          .catch(err => console.log(err));
+          .catch(err => setError(err.response.data));
     };
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Username</label>
-        <input
-          type='text'
-          name='username'
-          onChange={e =>
-            setCreds({ ...creds, [e.target.name]: e.target.value })
-          }
-        />
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={e =>
-            setPassword(e.target.value)
-          }
-        />
-        <label>Confirm Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={e =>
-            setConfirmPassword(e.target.value)
-          }
-        />
-      </div>
-      <button type='submit'>Sign Up</button>
-    </form>
+    <div>
+      {error && <p>{error.message}</p>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username</label>
+          <input
+            type='text'
+            name='username'
+            onChange={e =>
+              setCreds({ ...creds, [e.target.name]: e.target.value })
+            }
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type='password'
+            name='password'
+            onChange={e =>
+              setPassword(e.target.value)
+            }
+          />
+          <label>Confirm Password</label>
+          <input
+            type='password'
+            name='password'
+            onChange={e =>
+              setConfirmPassword(e.target.value)
+            }
+          />
+        </div>
+        <button type='submit'>Sign Up</button>
+      </form>
+    </div>
   );
 }
 
