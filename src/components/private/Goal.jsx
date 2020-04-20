a198b1edd97b19554029e95d4b084812a5c53929
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2020-04-20 14:39:31 -0600

    styling adjustments on goal cards

--- src/components/private/Goal.jsx
+++ src/components/private/Goal.jsx
@@ -4,10 +4,10 @@ import { Button } from 'reactstrap'
 function Goal(props) {
   return (
     <div className='goalContainer'>
-      <p>Goal: {props.card.item_name}</p>
-      <p>Category: {props.card.category_name}</p>
-      <p>Description: {props.card.description}</p>
-      <p>Finish by: {props.card.target_date}</p>
+      <p><b>Category:</b> {props.card.category_name}</p>
+      <p className='title'><b>Title:</b> {props.card.item_name}</p>
+      <p className='description'>{props.card.description}</p>
+      <p><b>Finish by:</b> {props.card.target_date}</p>
       <div className='buttonContainer'>
         <Button color='danger' onClick={() => props.handleDelete(props.card.id)}>Delete</Button >
         {props.card.complete ? (
